ccf4695f87dcd3bf6447e67318237fa8a9ad7f26
Matsadura <zidanditoo1@gmail.com>
2024-08-22 15:06:52 +0100

    [ADD] Task 6: Add Bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,28 @@
+-- Creates a stored procedure AddBonus
+-- that adds a new correction for a student
+-- DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+    )
+BEGIN
+    DECLARE project_id INT;
+    SELECT id
+    INTO project_id
+    FROM projects
+    WHERE name = project_name
+    LIMIT 1;
+
+    IF project_id is NULL THEN
+    INSERT INTO projects (name)
+    VALUES (project_name);
+    END IF;
+
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, project_id, score);
+END$$
+
+DELIMITER ;
